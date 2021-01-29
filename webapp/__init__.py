3e25d1850584efec38adec053e4e8ed02c2ccc4a
Alexey Pakhomov <alexeyt101@mail.ru>
2021-01-29 15:49:03 +0300

    Добавили поддержку базы данных Flask-SQLAlchemy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from webapp.model import db, News
 from webapp.weather import weather_by_city
 from webapp.python_org_news import get_python_news
 
@@ -7,11 +8,13 @@ from webapp.python_org_news import get_python_news
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
+
     @app.route('/')
     def index():
         page_title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
 
     return app
