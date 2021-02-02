8e723740293f5850d3a5f5b32cca01963142a3f6
Alexey Pakhomov <alexeyt101@mail.ru>
2021-02-02 22:00:33 +0300

    Добавлена модель пользователя и функционал авторизации

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, redirect, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
-from webapp.model import db, News
+from webapp.forms import LoginForm
+from webapp.model import db, News, User
 from webapp.weather import weather_by_city
 from webapp.python_org_news import get_python_news
 
@@ -10,11 +12,60 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+
     @app.route('/')
     def index():
         page_title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
+
+    
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+    
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password_hash(form.password.data):
+                login_user(user)
+                flash('Вы успешно вошли на сайт')
+                return redirect(url_for('index'))
+        flash('Неправильные имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+
+    
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return 'Привет админ!'
+        else:
+            return 'Вы не являетесь админом'
+
 
     return app
