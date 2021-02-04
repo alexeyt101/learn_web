4f54e82211792557708d82f6485b37242376dc9e
Alexey Pakhomov <alexeyt101@mail.ru>
2021-02-04 21:34:42 +0300

    Проведен рефакторинг, добавлены blueprints

--- webapp/user/models.py
+++ webapp/user/models.py
@@ -1,21 +1,10 @@
 from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
-
-class News(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    published = db.Column(db.DateTime, nullable=False)
-    text = db.Column(db.Text, nullable=True)
-
-    def __repr__(self):
-        return '<News {} {}>'.format(self.title, self.url)
+from webapp.db import db
 
 
-class User(db.Model, UserMixin  ):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(65), index=True, unique=True)
     password = db.Column(db.String(128))
